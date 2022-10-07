d711ef6ec88d810143a7b30655d1efcfbbf835e3
artmds <arthur.mdsousa@gmail.com>
2022-10-06 21:23:23 -0300

    criação do modal, editar e apagar task

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,9 @@ const taskController = require("../controller/taskController")
 
 routes.get("/", taskController.getAllTasks);
 routes.post("/create", taskController.createTask);
+routes.get("/getById/:id/:method", taskController.getById)
+routes.post("/updateOne/:id", taskController.updateTask)
+routes.get("/deleteOne/:id", taskController.deleteOneTask)
+
 
 module.exports = routes;
